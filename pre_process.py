8920fe2c1fedbb75004208f87166ffc3b10d9c90
Yang Liu <liuyang12@focusmedia.cn>
2019-08-29 13:51:27 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -1,91 +1,34 @@
-import argparse
 import os
 import pickle
 
-import pinyin
-from tqdm import tqdm
-
-from config import wav_folder, tran_file, data_file, char_to_idx, unk_id, speaker_info
-from utils import ensure_folder
+from config import data_file, thchs30_folder
 
 
-def get_data(split, gender):
-    print('getting {} data...'.format(split))
+# split in ['train', 'test', 'dev']
+def get_thchs30_data(split):
+    print('loading {} samples...'.format(split))
 
-    speaker_dict = get_speaker_dict()
-
-    with open(tran_file, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-
-    tran_dict = dict()
-    for line in lines:
-        tokens = line.split()
-        key = tokens[0]
-        trn = ''.join(tokens[1:])
-        tran_dict[key] = trn
+    data_dir = os.path.join(thchs30_folder, 'data')
+    wave_dir = os.path.join(thchs30_folder, split)
 
     samples = []
 
-    folder = os.path.join(wav_folder, split)
-    ensure_folder(folder)
-    dirs = [os.path.join(folder, d) for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
-    for dir in tqdm(dirs):
-        files = [f for f in os.listdir(dir) if f.endswith('.wav')]
+    for file in os.listdir(wave_dir):
+        file_path = os.path.join(wave_dir, file)
+        if file_path.endswith('.wav'):
+            text_path = os.path.join(data_dir, file + '.trn')
+            with open(text_path, 'r', encoding='utf-8') as f:
+                text = f.readlines()[1].strip()
+            samples.append({'audiopath': file_path, 'text': text})
 
-        for f in files:
-            audiopath = os.path.join(dir, f)
-            speaker_id = audiopath.split('/')[4]
-            speaker_gender = speaker_dict[speaker_id]
-
-            if speaker_gender != gender:
-                continue
-
-            key = f.split('.')[0]
-            if key in tran_dict:
-                text = tran_dict[key]
-                text = pinyin.get(text.strip(), format="numerical", delimiter=" ")
-                text = list(text)
-
-                temp = []
-                for token in text:
-                    if token in char_to_idx:
-                        temp.append(char_to_idx[token])
-                    else:
-                        temp.append(unk_id)
-
-                text = temp
-
-                samples.append({'text': text, 'audiopath': audiopath})
-
-    print('split: {}, num_files: {}'.format(split, len(samples)))
     return samples
 
 
-def get_speaker_dict():
-    with open(speaker_info, 'r') as file:
-        lines = file.readlines()
-
-    speaker_dict = {'S' + l.strip().split(' ')[0]: l.strip().split(' ')[1] for l in lines}
-    return speaker_dict
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Tacotron2')
-    parser.add_argument('--gender', default='M', type=str)
-    args = parser.parse_args()
-    return args
-
-
 def main():
-    args = parse_args()
-    gender = args.gender
-
-    print('gender: ' + gender)
-
     data = dict()
-    data['train'] = get_data('train', gender)
-    data['dev'] = get_data('dev', gender)
-    data['test'] = get_data('test', gender)
+    data['train'] = get_thchs30_data('train')
+    data['dev'] = get_thchs30_data('dev')
+    data['test'] = get_thchs30_data('test')
 
     with open(data_file, 'wb') as file:
         pickle.dump(data, file)
